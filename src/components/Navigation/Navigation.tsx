d75907f8566a833f71ec94bb0c5266ed3a9d57ad
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-07-24 11:49:54 +0200

    close navigation on click outside

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,24 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Hamburger from "hamburger-react";
-import { StyledNav, NavContent, HamburgerWrapper } from "components/Navigation/Navigation.style";
+import {
+  StyledNav,
+  NavContent,
+  HamburgerWrapper,
+} from "components/Navigation/Navigation.style";
 import mobileLogo from "assets/images/mobile-logo.png";
 import desktopLogo from "assets/images/desktop-logo.png";
 
 const Navigation = () => {
-  const [isOpen, setOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isMobile, setIsMobile] = useState(false);
 
   window.onresize = () => {
     setIsMobile(window.innerWidth < 768 ? true : false);
   };
 
+  const navRef: any = useRef(null);
+
+  const useOutsideAlerter = (ref: any) => {
+    useEffect(() => {
+      const handleClickOutside = (e: any) => {
+        if (ref.current && !ref.current.contains(e.target)) {
+          setIsOpen(false);
+        }
+      };
+      document.addEventListener("mousedown", handleClickOutside);
+
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  };
+
+  useOutsideAlerter(navRef)
+
   return (
     <StyledNav>
       <img src={isMobile ? mobileLogo : desktopLogo} alt="Gerna Group" />
       <HamburgerWrapper>
-        <Hamburger toggled={isOpen} toggle={setOpen} color={isOpen ? '#2A2A2A' : '#FAFAFA'} />
+        <Hamburger
+          toggled={isOpen}
+          toggle={setIsOpen}
+          color={isOpen ? "#2A2A2A" : "#FAFAFA"}
+        />
       </HamburgerWrapper>
-      <NavContent isOpen={isOpen}>
+      <NavContent isOpen={isOpen} ref={navRef}>
         <a href="#">Link 1</a>
         <a href="#">Link 1</a>
         <a href="#">Link 1</a>
