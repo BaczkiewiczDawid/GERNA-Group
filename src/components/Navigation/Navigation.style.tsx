646cfbde3f14bfa5e841e1929b84b99b584f5d2c
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-07-24 11:35:27 +0200

    mobile navigation content & nav animation

--- src/components/Navigation/Navigation.style.tsx
+++ src/components/Navigation/Navigation.style.tsx
@@ -1,13 +1,55 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+type NavType = {
+  isOpen: boolean;
+};
+
+const slide = keyframes`
+    0% {
+        width: 20vw;
+        opacity: 0;
+    }
+
+    100% {
+        width: 80vw;
+    }
+`;
 
 export const StyledNav = styled.nav`
-    width: 100vw;
-    height: 4rem;
-    background-color: ${({ theme }) => theme.colors.blue};
-    border-bottom-left-radius: .5rem;
-    border-bottom-right-radius: .5rem;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0 1rem;
+  width: 100vw;
+  height: 4rem;
+  background-color: ${({ theme }) => theme.colors.blue};
+  border-bottom-left-radius: 0.5rem;
+  border-bottom-right-radius: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 1rem;
+`;
+
+export const NavContent = styled.div<NavType>`
+  width: 80vw;
+  position: fixed;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.background};
+  right: 0;
+  top: 0;
+  -webkit-box-shadow: -6px 0px 28px -12px rgba(66, 68, 90, 1);
+  -moz-box-shadow: -6px 0px 28px -12px rgba(66, 68, 90, 1);
+  box-shadow: -6px 0px 28px -12px rgba(66, 68, 90, 1);
+  display: ${(props) => (props.isOpen ? "flex" : "none")};
+  justify-content: center;
+  flex-direction: column;
+  animation: ${slide} .25s alternate;
+
+  a {
+    margin: 1rem 2rem;
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.text};
+    cursor: pointer;
+  }
+`;
+
+export const HamburgerWrapper = styled.div`
+  z-index: 2;
 `;
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,23 +1,31 @@
-import { useState, useEffect } from 'react'
-import Hamburger from 'hamburger-react'
-import { StyledNav } from 'components/Navigation/Navigation.style';
-import mobileLogo from 'assets/images/mobile-logo.png';
-import desktopLogo from 'assets/images/desktop-logo.png';
+import { useState } from "react";
+import Hamburger from "hamburger-react";
+import { StyledNav, NavContent, HamburgerWrapper } from "components/Navigation/Navigation.style";
+import mobileLogo from "assets/images/mobile-logo.png";
+import desktopLogo from "assets/images/desktop-logo.png";
 
 const Navigation = () => {
-    const [isOpen, setOpen] = useState(false)
-    const [isMobile, setIsMobile] = useState(false);
+  const [isOpen, setOpen] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-    window.onresize = () => {
-        setIsMobile(window.innerWidth < 768 ? true : false);
-    }
+  window.onresize = () => {
+    setIsMobile(window.innerWidth < 768 ? true : false);
+  };
 
-    return (
-        <StyledNav>
-            <img src={isMobile ? mobileLogo : desktopLogo} alt="Gerna Group" />
-            <Hamburger toggled={isOpen} toggle={setOpen} color="#FAFAFA" />
-        </StyledNav>
-    )
-}
+  return (
+    <StyledNav>
+      <img src={isMobile ? mobileLogo : desktopLogo} alt="Gerna Group" />
+      <HamburgerWrapper>
+        <Hamburger toggled={isOpen} toggle={setOpen} color={isOpen ? '#2A2A2A' : '#FAFAFA'} />
+      </HamburgerWrapper>
+      <NavContent isOpen={isOpen}>
+        <a href="#">Link 1</a>
+        <a href="#">Link 1</a>
+        <a href="#">Link 1</a>
+        <a href="#">Link 1</a>
+      </NavContent>
+    </StyledNav>
+  );
+};
 
 export default Navigation;
