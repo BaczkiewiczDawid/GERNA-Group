ea73b3aaca3001a846568776093a4028d7851e1d
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-07-28 17:26:25 +0200

    total revenue chart styling

--- src/components/Dashboard/TotalRevenue.style.tsx
+++ src/components/Dashboard/TotalRevenue.style.tsx
@@ -11,15 +11,22 @@ export const ContentWrapper = styled.div`
   margin: 1rem auto;
   color: ${({ theme }) => theme.colors.text};
 
-  h2 {
-    margin-top: 0.5rem;
-    margin-bottom: 0.5rem;
-  }
-
   @media screen and (min-width: 768px) {
     width: 50vw;
     margin: 0;
     margin-top: 3rem;
     height: 25rem;
   }
+`;
+
+export const Title = styled.h2`
+  margin-top: 0.5rem;
+  margin-bottom: 1rem;
+  font-weight: 300;
+  font-size: 1rem;
+`;
+
+export const Revenue = styled.span`
+  font-weight: 700;
+  font-size: 1.75rem;
 `;
