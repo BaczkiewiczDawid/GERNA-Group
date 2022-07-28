08c722be9b57aa0ff615abe52e88650ab6209553
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-07-28 10:02:01 +0200

    total revenue chart

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,8 +1,59 @@
 import {
   Wrapper,
   InformationsContainer,
+  ContentWrapper,
+  StyledLineChart
 } from "components/Dashboard/Dashboard.style";
 import Informations from "components/Dashboard/Informations";
+import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
+
+const data = [
+  {
+    name: 'Jan',
+    revenue: '430234'
+  },
+  {
+    name: 'Feb',
+    revenue: '246857'
+  },
+  {
+    name: 'Mar',
+    revenue: '524156'
+  },
+  {
+    name: 'Apr',
+    revenue: '365248'
+  },
+  {
+    name: 'May',
+    revenue: '685424'
+  },
+  {
+    name: 'Jun',
+    revenue: '724653'
+  },
+  {
+    name: 'Jul',
+    revenue: '624578'
+  },
+  {
+    name: 'Aug',
+    revenue: '425632'
+  },
+  {
+    name: 'Sep',
+    revenue: '452687'
+  },
+  {
+    name: 'Oct',
+    revenue: '265983'
+  },
+  {
+    name: 'Nov',
+    revenue: '542658'
+  },
+
+]
 
 const Dashboard = () => {
   return (
@@ -13,6 +64,16 @@ const Dashboard = () => {
         <Informations />
         <Informations />
       </InformationsContainer>
+      <ContentWrapper>
+        <h2>Total revenue</h2>
+        <span>$746,245</span>
+        <StyledLineChart width={300} height={300} data={data}>
+          <Line type="monotone" dataKey="revenue" stroke="#8884d8" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+        </StyledLineChart>
+      </ContentWrapper>
     </Wrapper>
   );
 };
