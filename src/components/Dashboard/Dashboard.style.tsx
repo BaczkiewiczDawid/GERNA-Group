7d06a974bbd393a09055c26398717e26c0e66edf
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-07-28 10:11:01 +0200

    extract total revenue chart to separated component

--- src/components/Dashboard/Dashboard.style.tsx
+++ src/components/Dashboard/Dashboard.style.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { LineChart } from 'recharts';
 
 export const Wrapper = styled.div`
   @media screen and (min-width: 768px) {
@@ -27,31 +26,3 @@ export const InformationsContainer = styled.div`
   }
 `;
 
-export const ContentWrapper = styled.div`
-  width: 90vw;
-  height: 30rem;
-  -webkit-box-shadow: 0px 12px 40px -12px rgba(66, 68, 90, 1);
-  -moz-box-shadow: 0px 12px 40px -12px rgba(66, 68, 90, 1);
-  box-shadow: 0px 12px 40px -12px rgba(66, 68, 90, 1);
-  background-color: ${({ theme }) => theme.colors.darkerBackground};
-  padding: 1rem;
-  margin: 1rem auto;
-  color: ${({ theme }) => theme.colors.text};
-
-  h2 {
-    margin-top: .5rem;
-    margin-bottom: .5rem;
-  }
-
-  @media screen and (min-width: 768px) {
-    width: 50vw;
-    margin: 0;
-    margin-top: 3rem;
-  }
-`;
-
-export const StyledLineChart = styled(LineChart)`
-  width: 90%;
-  height: 20rem;
-  margin-top: 2rem;
-`;
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,79 +1,20 @@
 import {
   Wrapper,
   InformationsContainer,
-  ContentWrapper,
-  StyledLineChart
 } from "components/Dashboard/Dashboard.style";
 import Informations from "components/Dashboard/Informations";
-import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
-
-const data = [
-  {
-    name: 'Jan',
-    revenue: '430234'
-  },
-  {
-    name: 'Feb',
-    revenue: '246857'
-  },
-  {
-    name: 'Mar',
-    revenue: '524156'
-  },
-  {
-    name: 'Apr',
-    revenue: '365248'
-  },
-  {
-    name: 'May',
-    revenue: '685424'
-  },
-  {
-    name: 'Jun',
-    revenue: '724653'
-  },
-  {
-    name: 'Jul',
-    revenue: '624578'
-  },
-  {
-    name: 'Aug',
-    revenue: '425632'
-  },
-  {
-    name: 'Sep',
-    revenue: '452687'
-  },
-  {
-    name: 'Oct',
-    revenue: '265983'
-  },
-  {
-    name: 'Nov',
-    revenue: '542658'
-  },
-
-]
+import TotalRevenue from "components/Dashboard/TotalRevenue";
 
 const Dashboard = () => {
   return (
     <Wrapper>
-    <h1>GERNA Group Dashboard</h1>
+      <h1>GERNA Group Dashboard</h1>
       <InformationsContainer>
         <Informations />
         <Informations />
         <Informations />
       </InformationsContainer>
-      <ContentWrapper>
-        <h2>Total revenue</h2>
-        <span>$746,245</span>
-        <StyledLineChart width={300} height={300} data={data}>
-          <Line type="monotone" dataKey="revenue" stroke="#8884d8" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-        </StyledLineChart>
-      </ContentWrapper>
+      <TotalRevenue />
     </Wrapper>
   );
 };
