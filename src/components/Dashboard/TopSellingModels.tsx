45d4e733ec7fb79314c45fe16de6513bf12fd4ce
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-07-29 12:11:34 +0200

    top selling models mobile & desktop view

--- src/components/Dashboard/TopSellingModels.tsx
+++ src/components/Dashboard/TopSellingModels.tsx
@@ -1,16 +1,17 @@
 import ContentWrapper from 'components/Dashboard/ContentWrapper';
+import { Header, Table } from 'components/Dashboard/TopSellingModels.style';
 
 const TopSellingModels = () => {
   return (
     <ContentWrapper>
-      <div>
+      <Header>
         <h2>Top selling models</h2>
         <select name="time-period-filter" id="">
           <option value="yearly">Yearly</option>
           <option value="monthly">Monthly</option>
         </select>
-      </div>
-      <table>
+      </Header>
+      <Table>
         <th>Model</th>
         <th>Price</th>
         <th>Sold</th>
@@ -27,7 +28,25 @@ const TopSellingModels = () => {
             <td>246</td>
             <td>$11,154,56</td>
         </tr>
-      </table>
+        <tr>
+            <td>BMW M3</td>
+            <td>$53,547</td>
+            <td>246</td>
+            <td>$11,154,56</td>
+        </tr>
+        <tr>
+            <td>BMW M3</td>
+            <td>$53,547</td>
+            <td>246</td>
+            <td>$11,154,56</td>
+        </tr>
+        <tr>
+            <td>BMW M3</td>
+            <td>$53,547</td>
+            <td>246</td>
+            <td>$11,154,56</td>
+        </tr>
+      </Table>
     </ContentWrapper>
   );
 };
