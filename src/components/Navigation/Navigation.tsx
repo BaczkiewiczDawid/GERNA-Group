f753887588228a2860d0018082977c630e7b48f0
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-07-30 10:24:42 +0200

    navigation links

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -8,10 +8,13 @@ import {
 } from "components/Navigation/Navigation.style";
 import mobileLogo from "assets/images/mobile-logo.svg";
 import desktopLogo from "assets/images/desktop-logo.svg";
+import Links from 'components/Navigation/Links';
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768 ? true : false);
+  const [isMobile, setIsMobile] = useState(
+    window.innerWidth < 768 ? true : false
+  );
 
   window.onresize = () => {
     setIsMobile(window.innerWidth < 768 ? true : false);
@@ -48,16 +51,10 @@ const Navigation = () => {
         />
       </HamburgerWrapper>
       <NavContent isOpen={isOpen} ref={navRef}>
-        <a href="/">Link 1</a>
-        <a href="/">Link 1</a>
-        <a href="/">Link 1</a>
-        <a href="/">Link 1</a>
+        <Links />
       </NavContent>
       <NavDesktopContent>
-        <a href="/">Link 1</a>
-        <a href="/">Link 1</a>
-        <a href="/">Link 1</a>
-        <a href="/">Link 1</a>
+        <Links />
       </NavDesktopContent>
     </StyledNav>
   );
