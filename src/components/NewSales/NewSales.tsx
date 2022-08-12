8c2b6fa5a618ba7fc8b739b85896b2b3f2db52ca
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-08-12 11:15:57 +0200

    selecting car, department and employee

--- src/components/NewSales/NewSales.tsx
+++ src/components/NewSales/NewSales.tsx
@@ -1,23 +1,99 @@
+import { useState, useEffect } from "react";
 import Wrapper from "components/Dashboard/Wrapper";
 import ContentWrapper from "components/Dashboard/ContentWrapper";
-import Button from 'components/Employees/Button';
+import Button from "components/Employees/Button";
+import DepartmentsList from "data/DepartmentsList";
+import Axios from "axios";
+
+interface Car {
+  id: number;
+  manufactuer: string;
+  model: string;
+  price: number;
+  equipment: string[];
+}
+
+interface Employee {
+    id: number,
+    name: string,
+    age: number,
+    position: string,
+}
 
 const NewSales = () => {
+  const [carsList, setCarsList] = useState([]);
+  const [selectedDepartment, setSelectedDepartment] = useState("");
+  const [employeesList, setEmployeesList] = useState<Employee[]>([]);
+
+  const handleSelectedDepartment = (e: any) => {
+    setSelectedDepartment(e.target.value);
+  };
+
+  const getCarsList = () => {
+    Axios.get("http://localhost:3001/get-cars")
+      .then((response) =>
+        setCarsList(
+          response.data.sort((a: any, b: any) =>
+            a.manufactuer.localeCompare(b.manufactuer)
+          )
+        )
+      )
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getEmployeesList = () => {
+    Axios.post("http://localhost:3001/employees-list", {
+      department: selectedDepartment,
+    })
+      .then((response) => {
+        setEmployeesList(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getCarsList();
+    getEmployeesList();
+  }, [selectedDepartment]);
+
   return (
     <Wrapper>
       <h1>Add new sale</h1>
       <ContentWrapper>
         <label>Select car</label>
-        <select name="">
-          <option value="">Audi A4</option>
+        <select name="car">
+          {carsList.map((car: Car) => {
+            return (
+              <option key={car.id} value={car?.manufactuer + car?.model}>
+                {car?.manufactuer} {car?.model}
+              </option>
+            );
+          })}
         </select>
+
         <label>Department</label>
-        <select name="">
-          <option value="">Audi A4</option>
+        <select name="department" onChange={(e) => handleSelectedDepartment(e)}>
+          {DepartmentsList.map((department) => {
+            return (
+              <option key={department.name} value={department.name}>
+                {department.name}
+              </option>
+            );
+          })}
         </select>
         <label>Saler</label>
         <select name="">
-          <option value="">Audi A4</option>
+          {employeesList.map((employee: Employee) => {
+            return (
+              <option key={employee?.id} value={employee?.name}>
+                {employee?.name}
+              </option>
+            );
+          })}
         </select>
         <Button text="New sale" />
       </ContentWrapper>
---
+++ src/data/DepartmentsList.tsx
@@ -0,0 +1,28 @@
+const DepartmentsList = [
+  {
+    name: "Katowice",
+    link: "katowice",
+  },
+  {
+    name: "Wrocław",
+    link: "wroclaw",
+  },
+  {
+    name: "Olsztyn",
+    link: "olsztyn",
+  },
+  {
+    name: "Kraków",
+    link: "krakow",
+  },
+  {
+    name: "Warszawa",
+    link: "warszawa",
+  },
+  {
+    name: "Gdańsk",
+    link: "gdansk",
+  },
+];
+
+export default DepartmentsList;
