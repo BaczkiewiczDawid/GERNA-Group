a6c5c87377687207364f3b888daa38de0e284197
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-08-15 17:49:41 +0200

    modal tests

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -31,7 +31,7 @@ const Modal = ({ isOpen, message, type, setIsOpen }: ModalType) => {
           <Content onClick={handleCloseModal}>
             <Container>
               <img src={type === 0 ? SuccessIcon : ErrorIcon} alt={type === 0 ? 'success' : 'error'} />
-              <Message success={type === 0 ? true : false}>{message}</Message>
+              <Message data-testid="message" success={type === 0 ? true : false}>{message}</Message>
             </Container>
             <Timer success={type === 0 ? true : false}></Timer>
           </Content>
