7d07bebb4c346b9f24569fa8d2e6da04f5370d73
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-08-16 10:50:15 +0200

    initial login component

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,12 @@
+const Login = () => {
+    return (
+        <div>
+            <h1>Login to <span>GERNA</span></h1>
+            <input type="text" placeholder="E-Mail" />
+            <input type="password" placeholder="password" />
+            <button>Log in</button>
+        </div>
+    )
+}
+
+export default Login;
